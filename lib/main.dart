dae0d83d8fc1d103bec97e1cdd40b19968e4cd4e
Nordstrom <jakobnordstrom99@gmail.com>
2023-09-08 11:26:18 +0200

    Changed main, added three new files

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,96 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'add_task.dart';
+import 'listviewbuilder.dart';
+import 'theme.dart';
 
-void main() {
-  runApp(const MyApp());
+//Importerar flutter, google_fonts och add_task sidan.
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        centerTitle: true,
+        title: Text(
+          'To-Do List',
+          style: GoogleFonts.lato(fontSize: 32),
         ),
+        actions: [
+          Container(
+            margin: const EdgeInsets.only(right: 8.0),
+            child: InkWell(
+              onTap: () {
+                showDialog(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return AlertDialog(
+                      //Skapar en AlertDialog (liten box) med tre alternativ. För tillfället inte klickbara.
+                      scrollable: true,
+                      title: const Text('Filter'),
+                      content: Padding(
+                        padding: const EdgeInsets.all(8),
+                        child: Column(children: [
+                          Row(
+                            children: [
+                              Icon(Icons.check_box_outline_blank),
+                              Text('All'),
+                            ],
+                          ),
+                          Row(
+                            children: [
+                              Icon(Icons.check_box_outline_blank),
+                              Text('Not done'),
+                            ],
+                          ),
+                          Row(
+                            children: [
+                              Icon(Icons.check_box_outline_blank),
+                              Text('Done'),
+                            ],
+                          ),
+                        ]),
+                      ),
+                    );
+                  },
+                );
+              },
+              child: Stack(
+                //Knapp för att filtrera
+                children: [
+                  Center(
+                    child: Icon(Icons.sort),
+                  ),
+                ],
+              ),
+            ),
+          )
+        ],
       ),
+
+      body: ListViewBuilder(), //Kallar på en annan funktion i body.
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        onPressed: () {
+          Navigator.push(
+              context, //Navigator för att ta sig till andra sidan.
+              MaterialPageRoute(builder: (context) => const AddTask()));
+        },
+        child: Icon(Icons.add),
+      ),
     );
   }
+}
+
+void main() {
+  runApp(MaterialApp(
+    theme: appTheme,
+    home: MyApp(),
+  ));
 }
